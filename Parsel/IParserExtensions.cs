7e2fe74eb4e8a84705e4c2fdee9239ff1382a281
Admin <admin@initialround.com>
2013-01-13 15:14:43 -0800

    New extension methods

---
+++ Parsel/IParserExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parsel
+{
+    public static class IParserExtensions
+    {
+        public static IParser<T> ThenL<T, T1>(this IParser<T> p1, IParser<T1> p2)
+        {
+            return p1.Then(p2, (t, _) => t);
+        }
+
+        public static IParser<T1> ThenR<T, T1>(this IParser<T> p1, IParser<T1> p2)
+        {
+            return p1.Then(p2, (_, t) => t);
+        }
+
+        public static IParser<T> Between<T, T1>(this IParser<T> parser, IParser<T1> l, IParser<T1> r)
+        {
+            return l.ThenR(parser).ThenL(r);
+        }
+
+        public static IParser<T[]> SepBy<T, T1>(this IParser<T> parser, IParser<T1> sep)
+        {
+            return parser.Then(sep.ThenR(parser).Star(), (t, ts) => new[] { t }.Concat(ts).ToArray());
+        }
+    }
+}
